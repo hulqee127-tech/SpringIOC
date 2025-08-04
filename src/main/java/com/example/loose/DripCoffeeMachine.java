3dc93d19515f4ef3ff8336a2051bb10b27a309c6
hulqee127-tech <hulqee127@gmail.com>
2025-08-04 17:34:36 +0900

    first commit

---
+++ src/main/java/com/example/loose/DripCoffeeMachine.java
@@ -0,0 +1,10 @@
+package com.example.loose;
+
+public class DripCoffeeMachine implements CoffeeMachine {
+
+	@Override
+	public String brew() {
+		return "개드립커피머신으로커피추출하기";
+	}
+
+}
---
+++ src/main/java/com/example/loose/EspressoMachine.java
@@ -0,0 +1,10 @@
+package com.example.loose;
+
+public class EspressoMachine implements CoffeeMachine {
+
+	@Override
+	public String brew() {
+		return "에스프레소머신으로커피추출하기";
+	}
+	
+}
